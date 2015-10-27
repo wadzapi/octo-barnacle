d8ab8fa3dc5f5736081a354e9fe455f1e1de0814
WadZapi <wadzapi@gmail.com>
2015-10-27 22:04:20 +0300

    Поправлен файл gitignore

---
+++ src/CombinationGenerator.java
@@ -0,0 +1,19 @@
+/**
+ * Класс-генератор сочетаний из n по k
+ **/
+public class CombinationGenerator implements Iterator<Integer> {
+
+	@Override
+	public boolean hasNext() {
+		
+	}
+
+
+	@Override
+	public Integer next() {
+		return Integer.valueOf(0);
+	}
+	
+	
+	
+}
