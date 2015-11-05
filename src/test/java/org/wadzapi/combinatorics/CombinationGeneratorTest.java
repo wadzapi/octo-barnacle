ea7be140798344229228b9870c926e5d4ec5ac71
WadZapi <wadzapi@gmail.com>
2015-11-05 11:03:12 +0300

    Изменена структура каталогов и добавлена зависимость на JUnit для встраивания юнит-тестов и перехода к подходу TDD в разработке проекта

---
+++ src/test/java/org/wadzapi/combinatorics/CombinationGeneratorTest.java
@@ -0,0 +1,32 @@
+package org.wadzapi.combinatorics;
+
+import org.junit.Test;
+
+import java.util.Set;
+
+/**
+ * Юнит-тесты методов класса {@link org.wadzapi.combinatorics.CombinationGenerator}
+ */
+public class CombinationGeneratorTest {
+
+    /**
+     * Тест метода {@link CombinationGenerator#hasMoreElements()}
+     */
+    @Test
+    public void testHasMoreElements() throws Exception {
+    }
+
+    /**
+     * Тест метода {@link CombinationGenerator#nextElement()}
+     */
+    @Test
+    public void testNextElement() throws Exception {
+    }
+
+    /**
+     * Тест метода {@link CombinationGenerator#generatePowerSet(Set)}}
+     */
+    @Test
+    public void testGeneratePowerSet() throws Exception {
+    }
+}
---
+++ src/test/java/org/wadzapi/combinatorics/JohnsonTrotterPermutationTest.java
@@ -0,0 +1,23 @@
+package org.wadzapi.combinatorics;
+
+import org.junit.Test;
+
+/**
+ * Юнит-тесты методов класса {@link org.wadzapi.combinatorics.JohnsonTrotterPermutation}
+ */
+public class JohnsonTrotterPermutationTest {
+
+    /**
+     * Тест метода {@link JohnsonTrotterPermutation#hasMoreElements()}
+     */
+    @Test
+    public void testHasMoreElements() {
+    }
+
+    /**
+     * Тест метода {@link JohnsonTrotterPermutation#nextElement()}
+     */
+    @Test
+    public void testNextElement() {
+    }
+}
